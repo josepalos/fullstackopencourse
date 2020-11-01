d028eb0b7e20e8d58f854cd5f87e852611097447
Josep <josep.a.95@gmail.com>
2020-11-01 18:27:04 +0100

    eslint fixes

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -7,18 +7,18 @@ const mongoose = require("mongoose");
 
 logger.info("Connecting to", config.MONGODB_URI);
 mongoose.connect(config.MONGODB_URI, config.MONGODB_OPTIONS)
-    .then(() => {
-        logger.info("Connected to MongoDB");
-    })
-    .catch(error => {
-        logger.error("Error connecting to MongoDB:", error);
-    });
+	.then(() => {
+		logger.info("Connected to MongoDB");
+	})
+	.catch(error => {
+		logger.error("Error connecting to MongoDB:", error);
+	});
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/blogs', blogsRouter);
+app.use("/api/blogs", blogsRouter);
 
 module.exports = app;
