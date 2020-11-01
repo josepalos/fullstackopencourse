67c47ec2204dbe39bd18a366908421a0b5de4579
Josep <josep.a.95@gmail.com>
2020-11-01 11:35:18 +0100

    Completed exercise 3.12: created mongo.js script to add persons to the database

---
+++ part3/phonebook/backend/mongo.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const url = `mongodb://localhost/fullstackcourse`
+
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+    useCreateIndex: true
+});
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+});
+
+const Person = mongoose.model('Person', noteSchema);
+
+function add_new_person(name, phone) {
+    const new_person = new Person({
+        name: name,
+        phone: phone,
+    });
+
+    new_person.save().then(result => {
+        console.log(`Added person ${result.name} (${result.phone}) to phonebook`);
+        mongoose.connection.close();
+    });
+}
+
+function list_persons() {
+    Person.find({}).then(result => {
+        console.log("Phonebook")
+        console.log("=========")
+        result.forEach(person => {
+            console.log(`- ${person.name}: ${person.phone}`);
+        });
+        mongoose.connection.close();
+    });
+}
+
+if (process.argv.length < 3) {
+    list_persons();
+} else {
+    const name = process.argv[2];
+    const phone = process.argv[3];
+    add_new_person(name, phone);
+}
